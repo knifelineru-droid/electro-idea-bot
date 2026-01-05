ead64292a564cbe33f435ba51a57d75ce26cd774
knifelineru-droid <knifeline.ru@gmail.com>
2026-01-05 10:08:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import logging
 import os
 import random
+TOKEN = os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("BOT_TOKEN is not set")
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
